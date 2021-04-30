d32f5a61b62df49c179b614416f0141a5581a86f
shubham <shubham763116@gmail.com>
2021-05-01 00:47:58 +0530

    header and home styling

---
+++ src/Product.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Product.css";
+function Product() {
+    return (
+        <div className="product">
+            <div className="product-info">
+                <p>The lean startup</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product__rating">
+                    <p>⭐</p>
+                    <p>⭐</p>
+                    <p>⭐</p>
+                </div>
+            </div>
+            <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" alt=" "/>
+            <button>Add to Basket</button>
+        </div>
+    )
+}
+
+export default Product; 
