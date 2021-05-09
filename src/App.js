1788d7bc70153afb022fe3a9fd8ad3ba78e0b7b6
shubham <shubham763116@gmail.com>
2021-05-09 21:55:18 +0530

    designing login page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
+import Login from './Login';
 function App() {
   return (
     <Router>
       <div className="app">
-      <Header/>
+      
         <Switch>
+          <Route path="/login"> 
+            <Login />
+          </Route>
           <Route path="/checkout">
-            
-            <Checkout/>
+            <Header />
+            <Checkout />
           </Route>
           
           <Route path="/">
-            
-            <Home/>
+            <Header />
+            <Home />
           </Route>
         </Switch>
         
--- src/Header.js
+++ src/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
-import {Link} from 'react-router-dom';
-import {useStateValue} from './StateProvider';
+import { Link } from 'react-router-dom';
+import { useStateValue } from './StateProvider';
 
 function Header(){
-    const [{basket}, dispatch] = useStateValue();
+    const [{ basket }, dispatch] = useStateValue();
     return (
         <div className='header'>
             <Link to="/">
@@ -26,16 +26,19 @@ function Header(){
             </div>
             <div
             className='header__nav'>
-                <div className='header__option'>
-                    <span
-                    className='header__optionLineOne'>
-                        Hello Guest
-                    </span>
-                    <span
-                    className='header__optionLineTwo'>
-                        Sign in
-                    </span>
-                </div>
+                <Link to='/login'>
+                    <div className='header__option'> 
+                        <span
+                        className='header__optionLineOne'>
+                            Hello Guest
+                        </span>
+                        <span
+                        className='header__optionLineTwo'>
+                            Sign in
+                        </span>
+                    </div>
+                </Link>
+                
 
                 <div className='header__option'>
                 <span
