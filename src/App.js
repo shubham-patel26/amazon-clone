2f9ef00ecb5d6ba80ae4b96c774587ed5a932150
shubham <shubham763116@gmail.com>
2021-05-10 17:50:51 +0530

    designed payment page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import Checkout from './Checkout';
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 import Login from './Login';
+import Payment from './Payment';
 import { auth } from './firebase';
 import { useStateValue } from './StateProvider';
 
@@ -45,6 +46,9 @@ function App() {
           <Route path="/checkout">
             <Header />
             <Checkout />
+          </Route>
+          <Route path='/payment'> 
+            <Payment/>
           </Route>
           
           <Route path="/">
